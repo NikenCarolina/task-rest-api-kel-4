469dd7e2970d0d075e06516b76d10aa7db17595d
Irvan <89005766+irvansaputra-irsa@users.noreply.github.com>
2025-03-26 15:29:26 +0700

    update

--- task-2/src/routes/index.tsx
+++ task-2/src/routes/index.tsx
@@ -1,9 +1,10 @@
 import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
 
 function RoutesProvider() {
   return (
     <Routes>
-      <Route path="/" element={<p>Home</p>} />
+      <Route path="/" element={<Home />} />
       <Route path="/preview/:id" element={<p>Preview</p>} />
       <Route path="*" element={<b>Not Found</b>} />
     </Routes>
