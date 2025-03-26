9762a7a4d5ba25f2118fc25c487225cdd88962ec
Irvan <89005766+irvansaputra-irsa@users.noreply.github.com>
2025-03-26 20:43:49 +0700

    update

---
+++ task-2/src/interfaces/product.ts
@@ -0,0 +1,16 @@
+interface IProduct {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+interface IProductWithslide extends IProduct {
+  prev: string;
+  next: string;
+}
+
+export type { IProduct, IProductWithslide };
