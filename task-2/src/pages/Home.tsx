c092a172264077ea6a29988ba805de58beced358
Irvan <89005766+irvansaputra-irsa@users.noreply.github.com>
2025-03-27 09:30:41 +0700

    updt

--- task-2/src/pages/Home.tsx
+++ task-2/src/pages/Home.tsx
@@ -126,7 +126,7 @@ const Home = () => {
         className="min-h-screen"
         style={{
           backgroundImage:
-            "url(https://images.unsplash.com/photo-1506619216599-9d16d0903dfd?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+            "url(https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
         }}
       >
         <div className="w-full flex justify-end">
